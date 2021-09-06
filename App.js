305b0873349ba558373ff25922d51df620aed2d0
gwenaelle_merle <gwenaelle-merle_student2021@wilder.school>
2021-09-06 12:10:48 +0200

    creation dossier et fichiers

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import {Provider} from 'react-redux';
+
+import store from './src/redux/store';
+import RecipesDetailsScreen from './src/components/RecipesEpic/RecipesDetailsScreen';
+import RecipesListsScreen from './src/components/RecipesEpic/RecipesListsScreen';
 
 export default function App() {
   return (
+    <Provider store={store}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <RecipesListsScreen />
+      <RecipesDetailsScreen />
     </View>
+    </Provider>
   );
 }
 
---
+++ src/components/RecipesEpic/RecipesDetailsScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const RecipesDetailsScreen = () => {
+    return (
+        <View>
+            <Text>RecipesDetailsScreen</Text>
+        </View>
+    );
+};
+
+export default RecipesDetailsScreen;
