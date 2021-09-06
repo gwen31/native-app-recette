e57c425a86eea3ed0b3d487c1e431759cbf6f733
gwenaelle_merle <gwenaelle-merle_student2021@wilder.school>
2021-09-06 14:40:36 +0200

    mise en place de react-Navigation

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet} from 'react-native';
 import {Provider} from 'react-redux';
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
 
 import store from './src/redux/store';
 import RecipesDetailsScreen from './src/components/RecipesEpic/RecipesDetailsScreen';
 import RecipesListsScreen from './src/components/RecipesEpic/RecipesListsScreen';
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <Provider store={store}>
-    <View style={styles.container}>
-      <RecipesListsScreen />
-      <RecipesDetailsScreen />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="RecipesList" component={RecipesListsScreen} />
+          <Stack.Screen name="RecipesDetail" component={RecipesDetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
