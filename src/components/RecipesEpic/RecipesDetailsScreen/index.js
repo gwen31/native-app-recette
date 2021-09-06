c47d66bf6b21fac7c303a04112a4eaea64138936
gwenaelle_merle <gwenaelle-merle_student2021@wilder.school>
2021-09-06 16:03:14 +0200

    navigation entre les écrans

--- src/components/RecipesEpic/RecipesDetailsScreen/index.js
+++ src/components/RecipesEpic/RecipesDetailsScreen/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
 
-const RecipesDetailsScreen = () => {
+const RecipesDetailsScreen = ({navigation}) => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>RecipesDetailsScreen</Text>
+            <Button title="Revenir" onPress={()=> { 
+                navigation.goBack();
+            }} />
         </View>
     );
 };
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
 export default RecipesDetailsScreen;
--- src/components/RecipesEpic/RecipesListsScreen/index.js
+++ src/components/RecipesEpic/RecipesListsScreen/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
-const RecipesListsScreen = () => {
+const RecipesListsScreen = ({navigation}) => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>RecipesListsScreen</Text>
+            <Button onPress={()=> {
+                navigation.navigate("RecipesDetail");
+            }} title="Aller sur l'écran détails" />
         </View>
     );
 };
 
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
 export default RecipesListsScreen;
+
